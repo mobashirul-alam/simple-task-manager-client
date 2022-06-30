2e2117ed9acd551e80feeb2a2cfcdc8d190a0903
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-06-30 23:16:00 +0600

    responsive navbar

--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -1,60 +1,58 @@
 import React from 'react';
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
-    let activeStyle = {
-        textDecoration: "underline",
-        color: "skyblue"
-    };
-    let activeHomeStyle = {
-        color: "skyblue"
-    };
+    let menu = <>
+        <li>
+            <Link
+                to='/completedTasks'
+                className='md:mx-4 mx-2'
+            >
+                Completed Tasks
+            </Link>
+        </li>
+        <li>
+            <Link
+                to='/todo'
+                className='md:mx-4 mx-2'
+            >
+                To-Do
+            </Link>
+        </li>
+        <li>
+            <Link
+                to='/calendar'
+                className='md:mx-4 mx-2'
+            >
+                Calendar
+            </Link>
+        </li>
+    </>
 
     return (
-        <div className='bg-gray-700 text-white text-lg py-4'>
-            <nav className='flex justify-around'>
-                <div>
-                    <NavLink
-                        to='/home'
-                        style={({ isActive }) =>
-                            isActive ? activeHomeStyle : undefined
-                        }
-                    >
+        <>
+            <div class="navbar bg-base-100 lg:px-16">
+                <div class="navbar-start">
+                    <div class="dropdown">
+                        <label tabindex="0" class="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {menu}
+                        </ul>
+                    </div>
+                    <Link to="/" class="btn btn-ghost normal-case text-xl">
                         Simple Task Manager
-                    </NavLink>
+                    </Link>
                 </div>
-                <div className=''>
-                    <NavLink
-                        to='/completedTasks'
-                        className='mx-4'
-                        style={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }
-                    >
-                        Completed Tasks
-                    </NavLink>
-                    <NavLink
-                        to='/todo'
-                        className='mx-4'
-                        style={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }
-                    >
-                        To-Do
-                    </NavLink>
-                    <NavLink
-                        to='/calendar'
-                        className='mx-4'
-                        style={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }
-                    >
-                        Calendar
-                    </NavLink>
+                <div class="navbar-end hidden lg:flex">
+                    <ul class="menu menu-horizontal p-0">
+                        {menu}
+                    </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </>
     );
 };
 
